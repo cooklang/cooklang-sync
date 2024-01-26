82601ba39cf9ce5f0d9e04e04b4377594f4da5e2
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2024-01-26 13:55:37 +0000

    feat: add deleted property

--- src/schema.rs
+++ src/schema.rs
@@ -4,6 +4,7 @@ diesel::table! {
     file_records (id) {
         id -> Integer,
         jid -> Nullable<Integer>,
+        deleted -> Integer,
         path -> Text,
         format -> Text,
         modified_at -> TimestamptzSqlite,

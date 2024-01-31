4ab3bc38e202d7f191d696aadcd95966ace3b199
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2024-01-31 20:43:36 +0000

    feat: all remote methods

--- client/src/syncer/remote.rs
+++ client/src/syncer/remote.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use bytes::Bytes;
 
 type Result<T, E = reqwest::Error> = std::result::Result<T, E>;
 
@@ -28,19 +30,46 @@ impl Remote {
 }
 impl Remote {
 
-    fn upload(&self, chunk: String, content: String) {
+    pub async fn upload(&self, chunk: String, content: Vec<u8>) -> Result<()>{
+        self.client
+            .post(self.api_endpoint.clone() + "/chunks/" + &chunk)
+            .body(content)
+            .send()
+            .await?;
 
+        Ok(())
     }
 
-    fn download(&self, chunk: String, content: String) {
+    pub async fn download(&self, chunk: String) -> Result<Bytes>{
+        let response = self.client
+            .get(self.api_endpoint.clone() + "/chunks/" + &chunk)
+            .send()
+            .await?;
 
+        response.bytes().await
     }
 
     pub async fn list(&self, local_jid: i32) -> Result<Vec<ResponseFileRecord>> {
         let jid_string = local_jid.to_string();
 
         let res = self.client
-            .get(self.api_endpoint.clone() + "metadata" + "?jid=" + &jid_string)
+            .get(self.api_endpoint.clone() + "/metadata/list?jid=" + &jid_string)
+            .send()
+            .await?;
+
+        res.json().await
+    }
+
+    pub async fn commit(&self, path: &PathBuf, chunk_ids: &str, format: &str) -> Result<Vec<ResponseFileRecord>> {
+        let params = [
+            ("format", format),
+            ("chunk_ids", chunk_ids),
+            ("path", &path.to_string_lossy()),
+        ];
+
+        let res = self.client
+            .post(self.api_endpoint.clone() + "/metadata/commit")
+            .form(&params)
             .send()
             .await?;
 

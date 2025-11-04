4b2016ba53985f201f4334ec2c548750cf11037b
Alexey Dubovskoy <dubovskoy.a@gmail.com>
2025-11-04 15:51:13 +0000

    fix: resolve CooklangSyncClientFFI module dependency

--- Package.swift
+++ Package.swift
@@ -2,7 +2,27 @@
 import PackageDescription
 import class Foundation.ProcessInfo
 
-var package = Package(
+let useLocalXCFramework = ProcessInfo.processInfo.environment["USE_LOCAL_XCFRAMEWORK"] != nil
+
+var targets: [Target] = [
+    .target(
+        name: "CooklangSyncClient",
+        dependencies: [.target(name: useLocalXCFramework ? "CooklangSyncClientFFI_local" : "CooklangSyncClientFFI")],
+        path: "swift/Sources/CooklangSyncClient"),
+]
+
+if useLocalXCFramework {
+    targets.append(.binaryTarget(
+        name: "CooklangSyncClientFFI_local",
+        path: "swift/CooklangSyncClientFFI.xcframework"))
+} else {
+    targets.append(.binaryTarget(
+        name: "CooklangSyncClientFFI",
+        url: "https://github.com/cooklang/cooklang-sync/releases/download/client-v0.2.10/CooklangSyncClientFFI.xcframework.zip",
+        checksum: "6af279cb1417dbba1044228e016b658fcf3952abc058496319f592def831362e"))
+}
+
+let package = Package(
     name: "cooklang-sync-client",
     platforms: [
         .iOS(.v15),
@@ -10,29 +30,9 @@ var package = Package(
     products: [
         .library(
             name: "CooklangSyncClient",
-            targets: ["CooklangSyncClient", "CooklangSyncClientFFI"]),
+            targets: ["CooklangSyncClient"]),
     ],
     dependencies: [
     ],
-    targets: [
-        .target(
-            name: "CooklangSyncClient",
-            path: "swift/Sources/CooklangSyncClient"),
-        .binaryTarget(
-            name: "CooklangSyncClientFFI",
-            url: "https://github.com/cooklang/cooklang-sync/releases/download/client-v0.2.10/CooklangSyncClientFFI.xcframework.zip",
-            checksum: "6af279cb1417dbba1044228e016b658fcf3952abc058496319f592def831362e"),
-    ]
+    targets: targets
 )
-
-let cooklangSyncClientTarget = package.targets.first(where: { $0.name == "CooklangSyncClient" })
-
-if ProcessInfo.processInfo.environment["USE_LOCAL_XCFRAMEWORK"] == nil {
-    cooklangSyncClientTarget?.dependencies.append("CooklangSyncClientFFI")
-} else {
-    package.targets.append(.binaryTarget(
-        name: "CooklangSyncClientFFI_local",
-        path: "swift/CooklangSyncClientFFI.xcframework"))
-
-    cooklangSyncClientTarget?.dependencies.append("CooklangSyncClientFFI_local")
-}
